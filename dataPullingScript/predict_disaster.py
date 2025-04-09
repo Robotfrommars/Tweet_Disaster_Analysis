52cb55192e06b45000b73f9ec174f6a11017e762
hideyomoney <135862888+hideyomoney@users.noreply.github.com>
2025-04-09 17:43:13 -0500

    Update predict_disaster.py

--- dataPullingScript/predict_disaster.py
+++ dataPullingScript/predict_disaster.py
@@ -1,21 +1,20 @@
-
 import joblib
+
 
 class DisasterPredictor():
     def __init__(self):
-        self.vectorizer = joblib.load('../tfidf_vectorizer.pkl')
-        self.model = joblib.load('../lightgbm_model.pkl')
-        self.label_encoder = joblib.load('../label_encoder.pkl')
-        
-    def predict_disasters(self, db):
-        posts = db.get_all_posts()
-        print('Adding disaster types')
+        self.vectorizer = joblib.load('tfidf_vectorizer.pkl')
+        self.model = joblib.load('lightgbm_model.pkl')
+        self.label_encoder = joblib.load('label_encoder.pkl')
 
+    def predict_disasters_on_list(self, posts):
+        print('Adding disaster types to posts')
         for post in posts:
             self.add_disaster_type(post)
-        db.add_bluesky_posts(posts)
-        print('Finished adding disaster types')
-        
+        return posts
+
+
+
     def add_disaster_type(self, post):
         input_vector = self.vectorizer.transform([post.text])
         prediction = self.model.predict(input_vector).argmax(axis=1)[0]
