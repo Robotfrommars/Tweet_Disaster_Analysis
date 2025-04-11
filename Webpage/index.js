dfa81fbc25d61b8ccef02053892125bad1acb7f9
Zachary Valentine <75691890+Robotfrommars@users.noreply.github.com>
2025-04-11 12:36:28 -0500

    Update index.js

--- Webpage/index.js
+++ Webpage/index.js
@@ -23,11 +23,22 @@ const tweetSchema = new mongoose.Schema({
 
 const Tweet = mongoose.model("Tweet", tweetSchema);
 
+
 // GET home page & Fetch Tweets
 router.get('/', async function(req, res, next) {
   try {
     const tweets = await Tweet.find({disaster_type: { $in: ["tornado", "hurricane","earthquake","flood","wildfire","blizzard","haze","meteor"] } }).sort({ timestamp: -1 }).limit(500);
-    res.render('index', { title: 'Bluesky Tweets', tweets, mKey  });
+    let tCount = await Tweet.find({disaster_type: { $in: ["tornado"] } }).countDocuments();
+    let hCount = await Tweet.find({disaster_type: { $in: ["hurricane"] } }).countDocuments();
+    let eCount = await Tweet.find({disaster_type: { $in: ["earthquake"] } }).countDocuments();
+    let fCount = await Tweet.find({disaster_type: { $in: ["flood"] } }).countDocuments();
+    let wCount = await Tweet.find({disaster_type: { $in: ["wildfire"] } }).countDocuments();
+    let bCount = await Tweet.find({disaster_type: { $in: ["blizzard"] } }).countDocuments();
+    let zCount = await Tweet.find({disaster_type: { $in: ["haze"] } }).countDocuments();
+    let mCount = await Tweet.find({disaster_type: { $in: ["meteor"] } }).countDocuments();
+    let barData=[tCount,hCount,eCount,fCount,wCount,bCount,zCount,mCount];
+    console.log(tCount);
+    res.render('index', { title: 'Bluesky Tweets', tweets, mKey, barData  });
     
   } catch (error) {
     res.status(500).send("Error fetching tweets.");
